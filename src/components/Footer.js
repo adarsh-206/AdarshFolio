f9780362847e69bbdb65a8e090240c26b6783fcd
adarsh.dhote@qubited.com <adarsh.dhote@qubited.com>
2023-05-28 20:01:26 +0530

    complete portfolio

---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons';
+
+function Footer() {
+  return (
+    <div class="footer">
+          <div class="footer-column">
+            <h3>Phone Number</h3>
+            <p>+91 - 8878308624</p>
+          </div>
+          <div class="footer-column">
+            <h3>Email Address</h3>
+            <p>adarshdhote8624@gmail.com</p>
+          </div>
+          <div class="footer-column">
+            <h3>Follow Me</h3>
+            <div className='socialIcon'>
+              <SocialIcon className='social-icon'  url="https://www.linkedin.com/in/adarsh-dhote" bgColor="#0077B5"  style={{ height: 22, width: 22, margin: '0 5px' }} />
+              <SocialIcon className='social-icon' url="https://github.com/adarsh-206" bgColor="#333" style={{ height: 22, width: 22, margin: '0 5px' }} />
+              <SocialIcon className='social-icon' url="https://www.instagram.com/adarsh_206_" bgColor="#E1306C" style={{ height: 22, width: 22, margin: '0 5px' }} />
+              <SocialIcon className='social-icon' url="https://wa.me/+918878308624" bgColor="#25D366" style={{ height: 22, width: 22, margin: '0 5px' }} />
+            </div>
+          </div>
+          <div class="footer-column">
+            <p>Made with 
+            <span class="heart" onmouseover="this.classList.add('pulsate');" onmouseout="this.classList.remove('pulsate');"> ❤️ </span>
+            </p>
+            <p>By Adarsh Dhote</p>
+          </div>
+       </div>
+  )
+}
+
+export default Footer;
--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,7 @@ import cppImage from '../images/c++.png'
 import dsImage from '../images/dsa.png'
 import nodeImage from '../images/node.png'
 import database from '../images/database.png'
+import Footer from './Footer'
 
 const skills = [
   { name: 'HTML', image: htmlImage },
@@ -33,6 +34,46 @@ const Card = ({ skill }) => {
 };
 
 function Home() {
+  const styles = [
+    { '--i': 1 },
+    { '--i': 3 },
+    { '--i': 16 },
+    { '--i': 5 },
+    { '--i': 13 },
+    { '--i': 6 },
+    { '--i': 20 },
+    { '--i': 6 },
+    { '--i': 7 },
+    { '--i': 10 },
+    { '--i': 8 },
+    { '--i': 17 },
+    { '--i': 11 },
+    { '--i': 12 },
+    { '--i': 14 },
+    { '--i': 2 },
+    { '--i': 9 },
+    { '--i': 15 },
+    { '--i': 4 },
+    { '--i': 19 }
+  ];
+  const redirectToVeggieFreshGitHub = () => {
+    window.location.href = 'https://github.com';
+  };
+  const redirectToVeggieFresh = () => {
+    window.location.href = 'https://github.com';
+  };
+  const redirectToFilesMobGitHub = () => {
+    window.location.href = 'https://github.com';
+  };
+  const redirectToFilesMob = () => {
+    window.location.href = 'https://github.com';
+  };
+  const redirectToNotesAddaGitHub = () => {
+    window.location.href = 'https://github.com';
+  };
+  const redirectToNotesAdda = () => {
+    window.location.href = 'https://github.com';
+  };
   return (
     <>
       <NavBar />
@@ -72,15 +113,104 @@ function Home() {
       </div>
 
       <div><h1 className='about-text'>Skills</h1></div>
-      <div className="container">
+      <div className="skill-container">
       {skills.map((skill, index) => (
         <Card key={index} skill={skill} />
       ))}
     </div>
 
     <div><h1 className='about-text'>Projects</h1></div>
+    <div>
+    <div class="project-container">
+		<div class="box">
+			<div class="content">
+				<h3>VeggieFresh</h3>
+				<p>Developed a real-time vegetable delivery app for users to order fresh produce. Admins can track order status and payments. Implemented Socket.io for real-time communication between client and server, ensuring instant updates for users.</p>
+			  <p>Technologies Used :- MERN Stack, Socket.io</p>
+        <div className="button-container">
+          <button className="custom-button-yellow" onClick={redirectToVeggieFreshGitHub}>View Code</button>
+          <button className="custom-button-yellow" onClick={redirectToVeggieFresh}>Demo</button>
+        </div>
+      </div>
+		</div>
+		<div class="box">
+			<div class="content">
+				<h3>FilesMob</h3>
+				<p>A file sharing app with full stack capabilities. Users can upload files, share them via links or QR codes. Built APIs using Node.js (Multer) for file uploads. Implemented nodemailer for email sending and scheduled a script to delete old files automatically.</p>
+        <p>Technologies Used :- NodeJS ExpressJS MongoDB</p>	
+        <div className="button-container">
+          <button className="custom-button-pink" onClick={redirectToFilesMobGitHub}>View Code</button>
+          <button className="custom-button-pink" onClick={redirectToFilesMob}>Demo</button>
+        </div>		
+      </div>
+		</div>
+		<div class="box">
+			<div class="content">
+				<h3>NotesAdda</h3>
+				<p>Web-based system for sharing notes among students, teachers, and admins. Features include CRUD operations, multi-user access, note download/upload, and secure registration/login.</p>
+			  <p>Technologies Used :- HTML CSS JS PHP</p>
+        <div className="button-container">
+          <button className="custom-button-blue" onClick={redirectToNotesAddaGitHub}>View Code</button>
+          <button className="custom-button-blue" onClick={redirectToNotesAdda}>Demo</button>
+        </div>
+      </div>
+		</div>
+	</div>
+	<svg>
+      <filter id="wavy">
+        <feTurbulence x="0" y="0" id="turbulence" baseFrequency="0.01" numOctaves="5" seed="2">
+          <animate attributeName="baseFrequency" dur="60s" values="0.01;0.05;0.01" repeatCount="indefinite" />
+        </feTurbulence>
+        <feDisplacementMap in="SourceGraphic" scale="30" />
+      </filter>
+  	</svg>
+    </div>
 
-
+    <div><h1 className='about-text'>Get in Touch</h1></div>
+    <p style={{textAlign: 'center', letterSpacing: '0.5px', paddingBottom: '20px'}}>Let's have a cup of coffee and take a moment to savor the simple joys of life.</p>
+    <div class="contact-container">
+    <div class="column image-column">
+      <div className='container-contact'>
+        <div className='plate'></div>
+        <div className='cup'>
+          <div className='top'>
+            <div className='vapour'>
+            {styles.map((style, index) => (
+              <span key={index} style={style}></span>
+            ))}
+            </div>
+            <div className='circle'>
+              <div className='tea'>
+              </div>
+            </div>
+          </div>
+          <div className='handle'></div>
+        </div>
+      </div>
+    </div>
+    <div class="column form-column">
+    <div className="form-container">
+      <form>
+        <div className="form-group">
+          <input type="text" id="name" name="name" placeholder="Name" required />
+        </div>
+        <div className="form-group">
+          <input type="email" id="email" name="email" placeholder="Email" required />
+        </div>
+        <div className="form-group">
+          <input type="phone" id="phone" name="phone" placeholder="Mobile Number" required />
+        </div>
+        <div className="form-group">
+          <textarea id="message" name="message" placeholder="Message" required></textarea>
+        </div>
+        <div className="form-group" style={{textAlign: 'center'}}>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+    </div>
+  </div>
+  <Footer/>
     </> 
   )
 }
